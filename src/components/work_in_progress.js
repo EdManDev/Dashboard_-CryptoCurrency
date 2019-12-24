4b51ec92cee1a2d33324ae06cd7ec72a561a20e6
EdMan <edman@MBP.local>
2019-12-24 14:59:11 -0400

    commit

---
+++ src/components/work_in_progress.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class work_in_progress extends Component {
+  render() {
+    return (
+      <div>
+        <h1>it an work in progress</h1>
+      </div>
+    );
+  }
+}
+export default work_in_progress;
